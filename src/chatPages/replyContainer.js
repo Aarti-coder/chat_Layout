e2b5889dc486c19ec4836d2899739c86e2d56b42
Aarti Aggarwal <aarti@scoreme.in>
2019-11-15 20:11:17 +0530

    chat layout

---
+++ src/chatPages/replyContainer.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import signal from '../images/signalGrey.svg';
+import eye from '../images/eye.svg'
+import Profile_Img from '../images/Marina.png'
+import Profile_user1 from '../images/user1.png'
+import screen from '../images/screen.jpg'
+import laugh_emoji from '../images/emoji/smile.png'
+import heart_img from '../images/emoji/heart.png'
+import plus from '../images/add.svg'
+import audio_img from '../images/speaker.svg'
+import video_img  from '../images/vedeo.svg'
+import tts_img  from '../images/aud.svg'
+import pic_img  from '../images/picture.svg'
+import text_img  from '../images/textT.svg'
+import villa1 from '../images/villa.jpg'
+import villa2 from '../images/villa2.jpg'
+import villa3 from '../images/villa3.jpg'
+import villamain from '../images/villa3.jpg'
+class ReplyContainer extends React.Component{
+    
+        constructor(props){
+            super(props);
+            this.state = {
+                rotate:true
+            }
+        }
+
+        rotateAnimate(){
+            this.setState({
+                rotate: this.state.rotate ? false : true
+            })
+        }
+
+    render(){
+        return(
+            <div className="chat-box">
+                <div className="col-md-12  border-b">
+                    <ul className="nav ml-lg-auto dis_flx">
+                        <li className="nav-item head-text2 rep">
+                            <a className="nav-link p-t-b-10 rep_bor" href="#" >
+                                <img src={eye} width="25" height="25" alt="reply_img" className="pr-2"/>REPLIES
+                            </a>
+                        </li>
+                        <li className="nav-item head-text2">
+                            <a className="nav-link  p-t-b-0" href="#">
+                            <img src={signal}  alt="message"  width="20" height="20" className="pr-2"/>REPORTS</a>
+                        </li>
+                    </ul>  
+                </div>
+                <div className="col-md-12 text-center today-txt m-top-10">
+                    <p>Today</p>
+                </div>
+                <div className="chat-message text-right p-l-r-20">
+                    <div className="chat-message-text">
+                        <span>Hey Olivia  <img src={laugh_emoji} width="14" height="14"/>, Did you get a chance <br/>to see venue</span>
+                        <span className="msg_time_send">2Hr ago</span>
+                    </div>
+                    <img src={Profile_user1}  alt="profile" className="chat-img"/>
+                </div>
+                <div className="col-md-12">
+                    <div className="chat-message-text-no-arrw offset-md-1">
+                        <span>Hi robin, this seems like a great start  <img src={laugh_emoji} width="14" height="14"/></span>
+                    </div>   
+                </div>
+                <div className="col-md-12 m-top-20">
+                    <div className="row m0">
+                         <div className="col-md-1 flexImg pad-0">
+                            <img src={Profile_Img}  alt="profile" className="chat-img align-btm"/>
+                        </div>
+                        <div className="col-md-5 pad-0 height">
+                            <img src={villamain}  alt="send_img" className="chat-screen-img1"/>
+                            <img src={villamain}  alt="send_img" className="chat-screen-img"/>
+                            <span class="msg_time_send-left">2Hr ago</span>
+                        </div>
+                        <div className="col-md-2">
+                            <img src={villa3}  alt="send_img" width="50px" height="50px" className="sub-screen"/>
+                            <img src={villa1}  alt="send_img" width="50px" height="50px" className="sub-screen"/>
+                            <img src={villa2}  alt="send_img" width="50px" height="50px" className="sub-screen"/>
+                        </div>
+                    </div>
+                   
+                    <div className="smile-emg">
+                        <img src={heart_img}  alt="send_img" width="36" height="36" className="pos"/>
+                    </div>
+                    <div className="dots"></div>
+                </div>
+                <div className="col-md-12 chatting_box">
+                    <div className="row m0">
+                        <div className="pad-rgt-15 ">
+                            <button className="add-sign" onClick={()=> this.rotateAnimate()}>
+                                <img className={this.state.rotate ? "position transForm1" : "position transForm"} src ={plus} width="16" height="16"/>
+                            </button>
+                        </div>
+                        <div className="col-md-8 msg0">
+                            <p>Omg Olivia !!
+                                <img src={heart_img}  alt="send_img" width="13" height="13" className="m-l-r-5"/>
+                                <img src={heart_img}  alt="send_img" width="13" height="13" className="m-l"/>
+                                <img src={heart_img}  alt="send_img" width="13" height="13" className="m-l"/> 
+                                <img src={heart_img}  alt="send_img" width="13" height="13" className="m-l"/>    
+                            </p>    
+                        </div>
+                        <div className="col-md-3 msgBox text-right p-r-0">
+                            <div className="send-msg-box">
+                                <svg  width="15" height="15" viewBox="0 0 18 18" className="m-r-10">
+                                    <path fill="#FFF" fill-rule="nonzero" d="M.302 7.748l7.199 2.764 2.767 7.183c.07.184.243.305.436.305h.004a.466.466 0 0 0 .435-.297L17.965.666a.5.5 0 0 0-.098-.522.468.468 0 0 0-.506-.112L.298 6.849A.48.48 0 0 0 0 7.303c0 .196.122.377.302.445zm10.414 8.48l-2.3-5.969 7.818-7.805-5.518 13.774zm4.827-14.433L7.748 9.58 1.786 7.29l13.757-5.495z"/>
+                                </svg>
+                                <span>send</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {!this.state.rotate ? <div className="col-md-8 attches-menus">
+                        <ul className="list-inline">
+                            <li><img src ={audio_img}  width="15" height="15"/>
+                                <span>Audio</span>
+                            </li>
+                            <li>
+                                <img src ={video_img}  width="15" height="15"/>
+                                <span>Video</span> 
+                            </li>
+                            <li className="text_back">
+                                <img src ={text_img}  width="12" height="10"/>
+                                <span>Text</span> 
+                            </li>
+                            <li>
+                                <img src ={pic_img}  width="15" height="15"/>
+                                <span>Image</span> 
+                            </li>
+                            <li>
+                                <img src ={tts_img}  width="15" height="15"/>
+                                <span>TTS</span> 
+                            </li>
+                            <li>
+                                <img src ={audio_img}  width="15" height="15"/>
+                                <span>Audio and Text</span> 
+                            </li>
+                        </ul>
+                    </div>:null}
+                </div>
+            </div>
+        )}
+    }
+export default ReplyContainer;
---
+++ src/chatPages/sidebar.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Profile_Img from '../images/user1.png'
+import message from '../images/message.svg';
+import friends from '../images/friends.svg';
+import vid from '../images/video-camera.svg';
+
+
+import '../chatCss/main.scss'
+
+class Sidebar extends React.Component{
+    
+        constructor(props){
+            super(props);
+            this.state = {
+
+            }
+        }
+
+
+    render(){
+        return(
+            <div className="side_menu">
+                <div className="intro-container">
+                    <img src={Profile_Img} alt="Profile_img" className="side-profile-img"/>
+                    <p className="p-name">Robin Wang</p>
+                    <p className="p-msg">Life starts at the end of your <br /> comfort zone</p>
+                </div>
+                <div className="message-container m-top-30 ">
+                     <div className="btn-msg shadowBox">
+                         <div className="row m0">
+                             <div className="col-md-4 pad-0 text-right">
+                                <svg xmlns="http://www.w3.org/2000/svg"  version="1.1" id="Capa_1" x="0px" y="0px" width="15px" height="14px" viewBox="0 0 510 510" style={{enableBackground:"new 0 0 510 510"}} ><g><g>
+                                    <g id="messenger">
+                                        <path d="M459,0H51C22.95,0,0,22.95,0,51v459l102-102h357c28.05,0,51-22.95,51-51V51C510,22.95,487.05,0,459,0z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#ffff"/>
+                                    </g>
+                                    </g></g> 
+                                </svg>
+                            </div>
+                            <div className="col-md-8 text-left pad-0"><span>Messages</span></div>
+                         </div>
+                     </div>
+                     <div className="btn-chat col-md-9 offset-md-1">
+                        <p>Chats</p>
+                     </div>
+                     <div className="btn-send-msg col-md-10 offset-md-2">
+                        <p>Send Message</p>
+                     </div>
+                </div>
+                <div className="message-container group-container">
+                    <div className="btn-msg shadowBox1">
+                         <div className="row m0">
+                             <div className="col-md-4 pad-0 text-right">
+                                 <svg version="1.1" id="Capa_1" x="0px" y="0px"  viewBox="0 0 512 512" style={{enableBackground:"new 0 0 512 512"}} width="15px" ><g><g>
+                                        <g>
+                                            <path d="M155.327,57.142c-51.531,0-93.454,44.45-93.454,99.086c0,54.636,41.923,99.086,93.454,99.086s93.455-44.45,93.455-99.086    C248.782,101.592,206.859,57.142,155.327,57.142z" data-original="#000000" className="active-path" data-old_color="#000000" fill="#A9A9A9"/>
+                                        </g>
+                                        </g><g>
+                                            <g>
+                                                <path d="M367.798,71.321c-0.211,0-0.425,0.001-0.636,0.002c-21.626,0.179-41.826,9.31-56.878,25.713    c-14.788,16.113-22.829,37.37-22.644,59.854c0.186,22.484,8.577,43.605,23.628,59.473c15.17,15.991,35.265,24.773,56.651,24.773    c0.215,0,0.43-0.001,0.646-0.002c21.626-0.179,41.826-9.311,56.878-25.713c14.788-16.113,22.829-37.37,22.644-59.855    C447.702,108.972,411.747,71.321,367.798,71.321z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#A9A9A9"/>
+                                            </g>
+                                        </g><g>
+                                            <g>
+                                                <path d="M371.74,257.358h-7.76c-36.14,0-69.12,13.74-94.02,36.26c6.23,4.78,12.16,9.99,17.78,15.61    c16.58,16.58,29.6,35.9,38.7,57.42c8.2,19.38,12.88,39.8,13.97,60.83H512v-29.87C512,320.278,449.08,257.358,371.74,257.358z" data-original="#000000" className="active-path" data-old_color="#000000" fill="#A9A9A9"/>
+                                            </g>
+                                        </g><g>
+                                            <g>
+                                                <path d="M310.35,427.478c-2.83-45.59-25.94-85.69-60.43-111.39c-25.09-18.7-56.21-29.77-89.92-29.77h-9.34    C67.45,286.319,0,353.768,0,436.978v17.88h310.65v-17.88C310.65,433.788,310.55,430.618,310.35,427.478z" data-original="#000000" className="active-path" data-old_color="#000000" fill="#A9A9A9"/>
+                                            </g>
+                                    </g></g> 
+                                </svg>
+                             </div>
+                            <div className="col-md-8 text-left pad-0"><span>Groups</span></div>
+                         </div>
+                     </div>
+                     <div className="btn-chat col-md-9 offset-md-1 pad-25">
+                        <p>History</p>
+                     </div>
+                     <div className="btn-send-msg col-md-11 offset-md-1 pad-30">
+                        <p>Create Groups</p>
+                     </div>
+                </div>
+                <div className="message-container group-container">
+                    <div className="btn-msg shadowBox1">
+                        <div className="row m0">
+                            <div className="col-md-4 pad-0 text-right">
+                                <img src={vid} alt="video_calls" className="side-profile-img" height="14" width="15"/>
+                            </div>
+                        <div className="col-md-8 text-left pad-0"><span>Video Calls</span></div>
+                        </div>
+                     </div>
+                     <div className="btn-chat col-md-9 offset-md-1 pad-25">
+                        <p>History</p>
+                     </div>
+                     <div className="btn-send-msg pad-rgt-15">
+                        <p>Callss</p>
+                     </div>
+                </div>
+            </div>
+        )};
+    }
+
+export default Sidebar;
